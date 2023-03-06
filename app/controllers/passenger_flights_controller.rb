9b7f6e2912d441c1c71c0f2d30544a9652b87d50
KaraJH <karas.jones@yahoo.com>
2023-03-06 11:13:39 -0700

    feat: Airlines show page with passing test

--- app/controllers/passenger_flights_controller.rb
+++ app/controllers/passenger_flights_controller.rb
@@ -5,9 +5,4 @@ class PassengerFlightsController < ApplicationController
    
     redirect_to flights_path
   end
-
-  private 
-  def passenger_flight_params 
-    params.permit(:id, :flight_id)
-  end
 end
