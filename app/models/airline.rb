828be296fbd59caed269f192fe3552179e3b0acb
Adam Bailey <ab67319@gmail.com>
2023-03-06 11:05:08 -0700

    feat: create #unique_adult_passengers method

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -3,4 +3,8 @@ class Airline < ApplicationRecord
   has_many :passengers, through: :flights
 
   validates_presence_of :name
+
+  def unique_adult_passengers
+    self.passengers.distinct.where("age >= ?", 18)
+  end
 end
