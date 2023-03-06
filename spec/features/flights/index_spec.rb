bf388f5c3ce5b47bc80cac658b3e00094581dc2f
Sam Walker <sgwalker327@gmail.com>
2023-03-06 10:39:11 -0700

    Finish User Story 1

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -29,7 +29,29 @@ RSpec.describe '/flights/index', type: :feature do
         expect(page).to have_content("1045 - American")
       end
 
-      
+      it 'I see a list of passengers under each flight' do
+        @american = Airline.create!(name: 'American')
+        @flight1 = @american.flights.create!(number: "1283", date: "10/27/17", departure_city: "Bangkok", arrival_city: "Johannesburg")
+        @flight2 = @american.flights.create!(number: "1187", date: "10/27/17", departure_city: "Bangkok", arrival_city: "Hong Kong")
+        @flight3 = @american.flights.create!(number: "1045", date: "10/27/17", departure_city: "Bangkok", arrival_city: "Sydney")
+        @pam = Passenger.create!(name: 'Pam', age: 42)
+        @bill = Passenger.create!(name: 'Bill', age: 57)
+        @sue = Passenger.create!(name: 'Sue', age: 39)
+        @bret = Passenger.create!(name: 'Bret', age: 27)
+        @pam.passenger_flights.create!(flight: @flight1)
+        @bill.passenger_flights.create!(flight: @flight1)
+        @sue.passenger_flights.create!(flight: @flight2)
+        @bret.passenger_flights.create!(flight: @flight3)
+
+        visit "/flights"
+
+        within "#flight-#{@flight1.id}" do
+          expect(page).to have_content("Pam")
+          expect(page).to have_content("Bill")
+          expect(page).to_not have_content("Sue")
+          expect(page).to_not have_content("Bret")
+        end
+      end
     end
   end
 end
