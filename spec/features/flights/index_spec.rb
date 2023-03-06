3f7d392c6390473a239b41df586934ee3d6c3f39
Sam Walker <sgwalker327@gmail.com>
2023-03-06 09:39:02 -0700

    set up initial route for flight index and view for displaying flight numbers

---
+++ spec/features/flights/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe '/flights/index', type: :feature do
+  describe 'As a visitor' do
+    describe 'When I visit the flights index page' do
+      it 'I see a list of flight numbers' do
+        @american = Airline.create!(name: 'American')
+        @american.flights.create!(number: "1283", date: "10/27/17", departure_city: "Bangkok", arrival_city: "Johannesburg")
+        @american.flights.create!(number: "1187", date: "10/27/17", departure_city: "Bangkok", arrival_city: "Hong Kong")
+        @american.flights.create!(number: "1045", date: "10/27/17", departure_city: "Bangkok", arrival_city: "Sydney")
+
+        visit "/flights"
+
+        expect(page).to have_content("1283")
+        expect(page).to have_content("1187")
+        expect(page).to have_content("1045")
+      end
+
+      it 'I see an airline name next to each flight number' do
+        
+      end
+    end
+  end
+end
