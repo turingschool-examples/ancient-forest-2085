5018a39d94c01141537707da5d32e393b126af84
KaraJH <karas.jones@yahoo.com>
2023-03-06 11:12:25 -0700

    feat: All adult passengers method

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,8 @@
 class Airline < ApplicationRecord
   has_many :flights
+  has_many :passengers, through: :flights
+
+  def all_adult_passengers 
+   passengers.where('passengers.age >= ?', 18).distinct
+  end
 end
