49ee301450bb0f69ff324bd8d25d709ae8797162
KaraJH <karas.jones@yahoo.com>
2023-03-06 10:44:44 -0700

    get passenger flight id

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,11 @@
 class Passenger < ApplicationRecord
   has_many :passenger_flights 
   has_many :flights, through: :passenger_flights
+
+  def get_passenger_flight_id(flight)
+    passenger_flights.where(passenger_id: self.id)
+                     .where(flight_id: flight.id)
+                     .pluck(:id)
+                     .first
+  end
 end
