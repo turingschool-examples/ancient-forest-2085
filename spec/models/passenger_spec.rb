c5533e93ab236e2283d4f4fb6e4597a68d4f5f03
Adam Bailey <ab67319@gmail.com>
2023-03-06 09:17:23 -0700

    test: create association and validation model tests

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Airline, type: :model do
+RSpec.describe Passenger, type: :model do
   describe 'relationships' do
+    it { should have_many :flight_passengers }
     it { should have_many :flights }
-    it { should have_many :passengers }
+    it { should have_many :airlines }
   end
 
   describe 'validations' do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :age }
   end
 end
