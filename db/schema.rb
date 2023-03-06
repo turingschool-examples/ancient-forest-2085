39dd0e7d689da91da13846e378ae3c1cac7a4d7e
Melony Franchini <meltravelz@gmail.com>
2023-03-06 09:31:54 -0700

    Create passenger & flight_passenger tables & set up relationships to all

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_26_171421) do
+ActiveRecord::Schema.define(version: 2023_03_06_161942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "airlines", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "flight_passengers", force: :cascade do |t|
+    t.bigint "flight_id"
+    t.bigint "passenger_id"
+    t.index ["flight_id"], name: "index_flight_passengers_on_flight_id"
+    t.index ["passenger_id"], name: "index_flight_passengers_on_passenger_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -25,8 +34,19 @@ ActiveRecord::Schema.define(version: 2022_09_26_171421) do
     t.string "date"
     t.string "departure_city"
     t.string "arrival_city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["airline_id"], name: "index_flights_on_airline_id"
   end
 
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "flight_passengers", "flights"
+  add_foreign_key "flight_passengers", "passengers"
   add_foreign_key "flights", "airlines"
 end
