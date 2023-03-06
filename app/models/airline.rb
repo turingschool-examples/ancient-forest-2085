ae59149b723738c4490aef4ba2bfb8d2f9e0977b
Dawson Timmons <DawsonTimmons@gmail.com>
2023-03-06 11:56:42 -0700

    feature: Add instance method for unique airline passengers over or equal to the age of 18

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,10 @@
 class Airline < ApplicationRecord
   has_many :flights
+
+  def adult_passengers
+    flights.joins(:passengers)
+    .where('passengers.age >= 18')
+    .distinct
+    .pluck(:name)
+  end
 end
