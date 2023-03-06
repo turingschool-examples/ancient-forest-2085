edf70d1769087fdec6640c693bf9e7552642ef47
Matt Enyeart <menyeart1@gmail.com>
2023-03-06 11:15:34 -0700

    Add passenger flight delete route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   get '/flights', to: 'flights#index'
+
+  post '/passenger/:id/flight/:id', to: 'passenger_flights#destroy'
 end
