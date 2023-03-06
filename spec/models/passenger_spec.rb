6543ab20a45470a43692036776aec455dc93ca4f
Harrison Ryan <hwryan12@gmail.com>
2023-03-06 11:29:18 -0600

    feature: Create test for model method #adult_passengers

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -5,4 +5,33 @@ RSpec.describe Passenger, type: :model do
     it {should have_many :passenger_flights}
     it {should have_many(:flights).through(:passenger_flights)}
   end
+
+  describe "Class Methods" do
+    before(:each) do
+      @frontier = Airline.create(name: "Frontier")
+      @delta = Airline.create(name: "Delta")
+      
+      @den = @frontier.flights.create(number: "1727", date: "08/03/20", departure_city: "Denver", arrival_city: "Reno")
+      @hou = @frontier.flights.create(number: "1992", date: "07/01/20", departure_city: "Houston", arrival_city: "Tampa")
+      @nyc = @delta.flights.create(number: "2022", date: "01/01/23", departure_city: "New York City", arrival_city: "Cleveland")
+      
+      @joe = Passenger.create(name: "Joe Smith", age: 7)
+      @diana = Passenger.create(name: "Diana Smith", age: 29)
+      @andre = Passenger.create(name: "Andre D'lau", age: 41)
+      @levar = Passenger.create(name: "LeVar Burton", age: 66)
+      
+      PassengerFlight.create(flight: @den, passenger: @joe)
+      PassengerFlight.create(flight: @den, passenger: @diana)
+      PassengerFlight.create(flight: @hou, passenger: @andre)
+      PassengerFlight.create(flight: @hou, passenger: @diana)
+      PassengerFlight.create(flight: @hou, passenger: @joe)
+      PassengerFlight.create(flight: @nyc, passenger: @levar)
+    end
+
+    describe "#adult_passengers" do
+      it "returns a unique list of all passengers 18 or older" do
+        expect(Passenger.adult_passengers).to eq([@diana, @andre, @levar])
+      end
+    end
+  end
 end
