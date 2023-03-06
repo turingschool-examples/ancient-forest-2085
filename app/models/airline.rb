f447a7cd82fba416a6aa3e52ebdf1109aa6e47d6
Weston Sandfort <sandfortw@gmail.com>
2023-03-06 10:31:13 -0700

    add adult_passengers method for an airline

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -3,4 +3,7 @@ class Airline < ApplicationRecord
   has_many :flight_passengers, through: :flights
   has_many :passengers, through: :flight_passengers
 
+  def adult_passengers
+    passengers.where('passengers.age >= 18')
+  end
 end
