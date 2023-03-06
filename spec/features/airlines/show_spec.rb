c9ffb49acef27c7fe52ecb8313f6fcbfb9b5884f
Adam Bailey <ab67319@gmail.com>
2023-03-06 11:05:35 -0700

    test: create feature test for user story 3, airline's passengers

---
+++ spec/features/airlines/show_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+RSpec.describe "Airlines Show Page" do
+  before(:each) do
+    @airline = Airline.create!(name: 'Emirates')
+
+    @flight_1 = Flight.create!(airline: @airline, number: "1111", date: "03/06/23", departure_city: 'Denver', arrival_city: 'Charlotte')
+    @flight_2 = Flight.create!(airline: @airline, number: "2222", date: "03/07/23", departure_city: 'Charlotte', arrival_city: 'Denver')
+    @flight_3 = Flight.create!(airline: @airline, number: "3333", date: "03/08/23", departure_city: 'Denver', arrival_city: 'Chicago')
+    @flight_4 = Flight.create!(airline: @airline, number: "4444", date: "03/10/23", departure_city: 'Denver', arrival_city: 'Orlando')
+
+
+    @abdul = Passenger.create!(name: "Abdul", age: 31)
+    @adam = Passenger.create!(name: "Adam", age: 36)
+    @chris = Passenger.create!(name: "Chris", age: 35)
+    @kid = Passenger.create!(name: "Kid", age: 5)
+
+    FlightPassenger.create!(flight: @flight_1, passenger: @adam)
+    FlightPassenger.create!(flight: @flight_2, passenger: @abdul)
+    FlightPassenger.create!(flight: @flight_2, passenger: @adam)
+    FlightPassenger.create!(flight: @flight_2, passenger: @chris)
+    FlightPassenger.create!(flight: @flight_2, passenger: @kid)
+    FlightPassenger.create!(flight: @flight_3, passenger: @chris)
+    FlightPassenger.create!(flight: @flight_4, passenger: @abdul)
+    
+    visit airline_path(@airline)
+  end
+  context 'User Story 3' do
+    describe 'As a visitor' do
+      describe "When I visit an airline's show page" do
+        it "I can see a unique list of only adult passengers (age >= 18) that have flights on this airline" do
+          within("#airline_adult_passengers") {
+            expect(page).to have_content("Abdul")
+            expect(page).to have_content("Adam")
+            expect(page).to have_content("Chris")
+            expect(page).to_not have_content("Kid")
+          }
+        end
+      end
+    end
+  end
+end
--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -7,21 +7,20 @@ RSpec.describe "Flights Index Page" do
     @flight_1 = Flight.create!(airline: @airline, number: "1111", date: "03/06/23", departure_city: 'Denver', arrival_city: 'Charlotte')
     @flight_2 = Flight.create!(airline: @airline, number: "2222", date: "03/07/23", departure_city: 'Charlotte', arrival_city: 'Denver')
     @flight_3 = Flight.create!(airline: @airline, number: "3333", date: "03/08/23", departure_city: 'Denver', arrival_city: 'Chicago')
-    @flight_4 = Flight.create!(airline: @airline, number: "4444", date: "03/09/23", departure_city: 'Chicago', arrival_city: 'Denver')
-    @flight_5 = Flight.create!(airline: @airline, number: "5555", date: "03/10/23", departure_city: 'Denver', arrival_city: 'Orlando')
-    @flight_6 = Flight.create!(airline: @airline, number: "6666", date: "03/11/23", departure_city: 'Orlando', arrival_city: 'Denver')
+    @flight_4 = Flight.create!(airline: @airline, number: "4444", date: "03/10/23", departure_city: 'Denver', arrival_city: 'Orlando')
+
 
     @abdul = Passenger.create!(name: "Abdul", age: 31)
     @adam = Passenger.create!(name: "Adam", age: 36)
     @chris = Passenger.create!(name: "Chris", age: 35)
 
+    FlightPassenger.create!(flight: @flight_1, passenger: @adam)
     FlightPassenger.create!(flight: @flight_2, passenger: @abdul)
     FlightPassenger.create!(flight: @flight_2, passenger: @adam)
     FlightPassenger.create!(flight: @flight_2, passenger: @chris)
-    FlightPassenger.create!(flight: @flight_5, passenger: @abdul)
-    FlightPassenger.create!(flight: @flight_1, passenger: @adam)
     FlightPassenger.create!(flight: @flight_3, passenger: @chris)
-
+    FlightPassenger.create!(flight: @flight_4, passenger: @abdul)
+    
     visit flights_path
   end
 
@@ -52,8 +51,8 @@ RSpec.describe "Flights Index Page" do
             }
           }
 
-          within("#flight-#{@flight_5.id}") {
-            expect(page).to have_content("Flight Number: 5555 | Airline: Emirates")
+          within("#flight-#{@flight_4.id}") {
+            expect(page).to have_content("Flight Number: 4444 | Airline: Emirates")
             within("#passengers") {
               expect(page).to have_content("Abdul")
             }
