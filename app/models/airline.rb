586ce136d3f35c2fbd8963b4fbae92cadccb18e5
Bobby Luly <bobbyluly@gmail.com>
2023-03-06 11:42:35 -0700

    Feat: create #unique_adult_name instance method

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -2,4 +2,8 @@ class Airline < ApplicationRecord
   has_many :flights
   has_many :flight_passengers, through: :flights
   has_many :passengers, through: :flight_passengers
+
+  def unique_adult_names
+    passengers.where("passengers.age > 18").distinct.order(:name).pluck(:name)
+  end
 end
