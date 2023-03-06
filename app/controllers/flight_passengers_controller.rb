9aa893bca0ef4b3143a7b25476165045517904d3
Andra Helton <andrahelton@gmail.com>
2023-03-06 11:53:47 -0700

    Add efforts to figure out us 2 destroy controller issue

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -1,7 +1,15 @@
 class FlightPassengersController < ApplicationController
   def destroy
+    # require 'pry'; binding.pry
+    @flight = Flight.find(params[:flight_id])
     @passenger = FlightPassenger.find(params[:id])
     @passenger.destroy
     redirect_to "/flights"
+  end
+
+  private
+
+  def flight_passenger_params
+    params.permit(:flight_id)
   end
 end
