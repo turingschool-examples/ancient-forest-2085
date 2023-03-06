5fb247a90abc12d642eebf24a2fe03953c436a21
Harrison Ryan <hwryan12@gmail.com>
2023-03-06 12:35:59 -0600

    feature: Complete extension

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,9 +3,12 @@ class Passenger < ApplicationRecord
   has_many :flights, through: :passenger_flights
 
   def self.adult_passengers(airline)
+    # binding.pry
     joins(flights: :airline)
+    select("passengers.*, COUNT(passenger_flights.id) AS flight_count")
+      .joins(:passenger_flights => {:flight => :airline})
       .where("airlines.id = ? AND passengers.age >= ?", airline.id, 18)
-      .select("passengers.*")
-      .distinct
+      .group("passengers.id")
+      .order("flight_count DESC")
   end 
 end
