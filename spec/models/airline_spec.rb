c0372fd209c6cefac93cf31608c913b7d2cac078
Jade Stewart <jadekstewart3@gmail.com>
2023-03-06 11:55:56 -0700

    Create unique passengers instance method

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -4,5 +4,24 @@ RSpec.describe Airline, type: :model do
   describe 'relationships' do
     it {should have_many :flights}
     it {should have_many(:passengers).through(:flights)}
+    
+    before (:each) do
+      @airline_1 = Airline.create!(name: "Frontier")
+      @flight = @airline_1.flights.create!(number: "1727", date: "08/03/20", departure_city: "Denver", arrival_city: "Reno")
+      @jade = Passenger.create!(name: "Jade", age: 25)
+      @hannah = Passenger.create!(name: "Hannah", age: 25)
+      @hannah_1 = Passenger.create!(name: "Hannah", age: 26)
+      @joe = Passenger.create!(name: "Joe", age: 17)
+      @joe = Passenger.create!(name: "Joe", age: 18)
+      PassengerFlight.create!(passenger: @jade, flight: @flight, status: 0)
+      PassengerFlight.create!(passenger: @hannah, flight: @flight, status: 0)
+      PassengerFlight.create!(passenger: @joe, flight: @flight, status: 0)
+      PassengerFlight.create!(passenger: @joe, flight: @flight, status: 0)
+      PassengerFlight.create!(passenger: @hannah_1, flight: @flight, status: 0)
+    end
+
+    it "shows unique passengers over 18" do
+      expect(@airline_1.unique_passengers).to eq(["Hannah", "Jade", "Joe"])
+    end
   end
 end
