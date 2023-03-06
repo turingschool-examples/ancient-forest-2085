978287904cc138f4282549eb7d4e55bfafde7a4a
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-03-06 10:48:41 -0700

    Add #sorted_unique_adult_passengers method to airline model

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -5,4 +5,12 @@ class Airline < ApplicationRecord
 	def unique_adult_passengers
 		passengers.where("passengers.age >= 18").distinct
 	end
+
+	def sorted_unique_adult_passengers
+		passengers
+		.where("passengers.age >= 18")
+		.select("passengers.*, COUNT(flights.*) as flight_count")
+		.group(:id)
+		.order(flight_count: :desc)
+	end
 end
--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Airline, type: :model do
 	let!(:flight_1) { united.flights.create!(number: 1727, date: '08/30/22', departure_city: 'Denver', arrival_city: 'Reno') }
 	let!(:flight_2) { united.flights.create!(number: 1832, date: '08/30/22', departure_city: 'Los Angeles', arrival_city: 'Austin') }
 	let!(:flight_3) { united.flights.create!(number: 1655, date: '08/30/22', departure_city: 'Phoenix', arrival_city: 'Las Vegas') }
-	let!(:flight_4) { frontier.flights.create!(number: 1726, date: '08/30/22', departure_city: 'New York', arrival_city: 'Seattle') }
+	let!(:flight_4) { united.flights.create!(number: 1726, date: '08/30/22', departure_city: 'New York', arrival_city: 'Seattle') }
 	let!(:flight_5) { frontier.flights.create!(number: 1690, date: '08/30/22', departure_city: 'Portland', arrival_city: 'San Diego') }
 
 	let!(:passenger_1) { Passenger.create!(name: 'Joe', age: 30) }
@@ -26,23 +26,38 @@ RSpec.describe Airline, type: :model do
 
 	before do
 		FlightPassenger.create!(flight: flight_1, passenger: passenger_1)
+		FlightPassenger.create!(flight: flight_1, passenger: passenger_3)
 		FlightPassenger.create!(flight: flight_1, passenger: passenger_4)
 		FlightPassenger.create!(flight: flight_1, passenger: passenger_6)
 
 		FlightPassenger.create!(flight: flight_2, passenger: passenger_1)
 		FlightPassenger.create!(flight: flight_2, passenger: passenger_2)
 		FlightPassenger.create!(flight: flight_2, passenger: passenger_3)
+		FlightPassenger.create!(flight: flight_2, passenger: passenger_5)
 
+		FlightPassenger.create!(flight: flight_3, passenger: passenger_1)
 		FlightPassenger.create!(flight: flight_3, passenger: passenger_3)
 		FlightPassenger.create!(flight: flight_3, passenger: passenger_4)
 		FlightPassenger.create!(flight: flight_3, passenger: passenger_5)
+
+		FlightPassenger.create!(flight: flight_4, passenger: passenger_3)
 
 		FlightPassenger.create!(flight: flight_5, passenger: passenger_7)
 	end
 
 	describe '#instance_methods' do
-		it '#unique_passengers' do
-			expect(united.unique_adult_passengers).to eq([passenger_1, passenger_3, passenger_5, passenger_6])
+		it '#unique_adult_passengers' do
+			expect(united.unique_adult_passengers).to match_array([passenger_1, passenger_3, passenger_5, passenger_6])
+		end
+
+		it '#sorted_unique_adult_passengers' do
+			expect(united.sorted_unique_adult_passengers).to eq([passenger_3, passenger_1, passenger_5, passenger_6])
+
+			FlightPassenger.create!(flight: flight_2, passenger: passenger_6)
+			FlightPassenger.create!(flight: flight_3, passenger: passenger_6)
+			
+			expect(united.sorted_unique_adult_passengers).to eq([passenger_3, passenger_1, passenger_6, passenger_5])
+
 		end
 	end
 end
