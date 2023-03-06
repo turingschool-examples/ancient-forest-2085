88e7bc021c7f7daff8a35a0286f063611ea50df8
Melony Franchini <meltravelz@gmail.com>
2023-03-06 11:18:16 -0700

    Create feat test, route, controller for US3

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get "/flights", to: "flights#index"
 
   delete "/flights/:flight_id/passengers/:id", to: "flight_passengers#destroy"
+
+  get "/airlines/:id", to: "airlines#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ spec/features/airlines/show_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "/airlines/:id", type: :feature do
+  describe "As a visitor, when I visit the airlines show page" do
+    before(:each) do
+      @southwest = Airline.create!(name: "Southwest Airlines")
+
+      @sw_flight1 = @southwest.flights.create!(number: "55", date: "01/01/2023", departure_city: "Denver", arrival_city: "Busan")
+      @sw_flight2 = @southwest.flights.create!(number: "101", date: "02/02/2023", departure_city: "Busan", arrival_city: "Istanbul")
+
+      @mel_passenger = Passenger.create!(name: "Mel", age: 38)
+      FlightPassenger.create!(flight: @sw_flight1, passenger: @mel_passenger)
+      FlightPassenger.create!(flight: @sw_flight2, passenger: @mel_passenger)
+      
+      @serap_passenger = Passenger.create!(name: "Serap", age: 31)
+      FlightPassenger.create!(flight: @sw_flight1, passenger: @serap_passenger)
+      FlightPassenger.create!(flight: @sw_flight2, passenger: @serap_passenger)
+
+      @serin_passenger = Passenger.create!(name: "Serin", age: 4)
+      FlightPassenger.create!(flight: @sw_flight1, passenger: @serin_passenger)
+
+
+      visit "/airlines/#{@southwest.id}"
+    end
+
+    #User Story 3
+    it "I see a UNIQUE list of (ADULT ONLY) passengers that have flights on that airline" do
+      expect(page).to have_content("#{@mel_passenger.name}", count: 1)
+      expect(page).to have_content("#{serap_passenger.name}", count: 1)
+
+      expect(page).to_not have_content("#{@serin_passenger.name}")
+    end
+  end
+end
