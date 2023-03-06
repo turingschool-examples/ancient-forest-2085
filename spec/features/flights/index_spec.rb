4ddd4cabac913d60b5c634f5793f75f439683280
Chris Crane <chrisjcrane@gmail.com>
2023-03-06 09:55:05 -0700

    complete second test for User Story 1

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Flights Index Page" do
 
     @flight_1 = @airline_1.flights.create!(number: "47", date: "08/03/22", departure_city: "Moscow", arrival_city: "Paris")
     @flight_2 = @airline_1.flights.create!(number: "29", date: "05/06/22", departure_city: "Rome", arrival_city: "Jerusalem")
-    @flight_3 = @airline_1.flights.create!(number: "876", date: "05/06/22", departure_city: "Somewhere", arrival_city: "Nowhere") #Control
+    @flight_3 = @airline_1.flights.create!(number: "369", date: "09/03/06", departure_city: "Somewhere", arrival_city: "Nowhere") #Control
 
     visit flights_path
   end
@@ -25,7 +25,15 @@ RSpec.describe "Flights Index Page" do
       end
 
       it "And next to each flight number I see the name of the Airline of that flight" do
+        within "#flight_#{@flight_1.id}" do
+          expect(page).to have_content("- #{@airline_1.name}")
+        end
 
+        within "#flight_#{@flight_2.id}" do
+        expect(page).to have_content("- #{@airline_1.name}")
+        end
+
+        expect(page).to_not have_content(@airline_2.name)
       end
 
       it "And under each flight number I see the names of all that flight's passengers" do
