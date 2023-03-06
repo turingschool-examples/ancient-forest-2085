bd2512078654dc65d9476cf375e01be2e0a94cb8
Adam Bailey <ab67319@gmail.com>
2023-03-06 09:17:33 -0700

    feat: create model associations and validations

---
+++ app/models/passenger.rb
@@ -0,0 +1,7 @@
+class Passenger < ApplicationRecord
+  has_many :flight_passengers
+  has_many :flights, through: :flight_passengers
+  has_many :airlines, through: :flights
+
+  validates_presence_of :name, :age
+end
