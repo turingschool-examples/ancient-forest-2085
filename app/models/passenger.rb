f8bb11348c95de2a7c6d2d024494f4276d876e6f
Dawson Timmons <DawsonTimmons@gmail.com>
2023-03-06 09:20:13 -0700

    Add database setup for many to many relationship

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,6 +1,4 @@
-class Flight < ApplicationRecord
-  belongs_to :airline
-
+class Passenger < ApplicationRecord
   has_many :flight_passengers
   has_many :passengers, through: :flight_passengers
 end
